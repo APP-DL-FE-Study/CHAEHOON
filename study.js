60460682a7223cfbee68d74adad1dcc516d8377f
CHAEHOON <ch05108@naver.com>
2022-05-24 18:58:22 +0900

    동적 엘레먼트 생성

--- study.js
+++ study.js
@@ -1,13 +1,27 @@
-const btn =document.querySelector('.submit');
-btn.addEventListener('click',showConsole);
-function showConsole() {
-    console.log(btn);
-    
-}
-const input = document.getElementById('in');
-input.addEventListener('change',getInput);
+const body = document.querySelector('body');
+
+
+
+const btn = document.querySelector('.submit');
+const inputElem =document.querySelector('#in');
+
+let value = "";
+
+btn.addEventListener('click', getInput);
 
 function getInput(event) {
-    console.log(event.target.value);
-    input.value= ""
+    value = inputElem.value
+    const textElem = document.createElement('span');
+    const container = document.createElement('div');
+    const btn1 = document.createElement('button');
+    btn1.innerHTML="삭제"
+    textElem.innerHTML = value;
+
+    
+
+    container.appendChild(textElem);
+    container.appendChild(btn1);
+    body.appendChild(container);
+
+    inputElem.value = "";
 }
